6f0aefcccea5872913d08e33e71439cd34632f46
Joe Smith <falangjoe@yahoo.com>
2017-10-29 15:13:48 -0600

    first working version

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -1,63 +1,72 @@
 import csv
 import cv2
+import random
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-    
-def load_log_file(file):
+
+
+def read_frames(file):
     with open(file) as csvfile:
         reader = csv.reader(csvfile)
-        result = [[line[0],line[1],line[2],float(line[3])] for line in reader]
-        return result
+        for line in reader:
+            position = float(line[3])
+            angle = 0.25 * (abs(position) // 0.01)
+            middle,left,right = line[0],line[1],line[2]
+            yield (angle, position, middle)
+            yield (angle, position + 0.25, left)
+            yield (angle, position - 0.25, right)
+            #yield (angle, position, middle, left, right)
+
+def group_frames(frames):
+    angles = list(map(lambda v : v[0], frames))
+    keys = list(set(angles))
+    samples = dict((k, []) for k in keys)
+    for frame in frames:
+        samples[frame[0]].append(frame)
+    return samples
+
+def pick_frames(samples):
     
-def load_log_files():
-    folders = [6]
-    result = []
-    
-    for ii in folders:
-        file = './data/{0}/driving_log.csv'.format(ii)
-        log = load_log_file(file) 
-        result += log
-    return result
-    
+    sample_count = 600
+
+    picked_frames = []
+    for key in samples.keys():
+        frames = samples[key]
+        indicies = np.random.permutation(np.arange(len(frames)))[0:sample_count]
+        for ii in indicies:
+            picked_frames.append(frames[ii])
+            
+    return picked_frames
+
 def flip_frame(frame):
     image_flipped = np.fliplr(frame[0])
     angle_flipped = -frame[1]
     return [image_flipped,angle_flipped]
 
+def load_frame(frame):
+    center_position = frame[1]
+    center_image = cv2.imread(frame[2])
+    center_frame = [center_image,center_position]
+    if random.choice([True, False]):
+        return center_frame
+    else:
+        return flip_frame(center_frame)
+    
+def pick_training_data():
+    all_frames = list(read_frames('./data/6/driving_log.csv'))
+    samples =  group_frames(all_frames) 
+    frames = pick_frames(samples)
+    return frames
+    
 
-def load_frames(log):
-    
-    offset = 0.1 #* (1.0 / 25.0)
-    
-    center_angle = log[3]
-    center_image = cv2.imread(log[0])
-    center_frame = [center_image,center_angle]
-    yield center_frame
-    yield flip_frame(center_frame)
- 
-    
-    #left_image = cv2.imread(log[1])
-    #left_angle = center_angle - offset
-    #left_frame = [left_image,left_angle] 
-    #yield left_frame
-##
-    #right_image = cv2.imread(log[2])
-    #right_angle = center_angle + offset
-    #right_frame = [right_image,right_angle]
-    #yield right_frame
-  
-    
-    
-train_logs,valid_logs = train_test_split(load_log_files(),test_size=0.1)
+train_logs,valid_logs = train_test_split(pick_training_data(),test_size=0.1)
 
 def generate_frames(logs):
     logs = shuffle(logs)
     for log in logs:
-        frames = load_frames(log)
-        for frame in frames:
-            yield frame
-            
+        frame = load_frame(log)
+        yield frame
             
 batch_size = 128
             
@@ -70,6 +79,8 @@ def generator(logs):
         targets = []
         
         for frame in frames:
+            #print('------------')
+            #print(frame)
             inputs.append(frame[0])
             targets.append(frame[1])
             if(len(targets) >= batch_size):
@@ -77,9 +88,9 @@ def generator(logs):
                 inputs = []
                 targets = []
                 
-
-            
                 
+
+                                
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import Conv2D, Cropping2D, Dropout
@@ -103,14 +114,30 @@ model.add(Dense(1 * 10,activation="relu"))
 #model.add(Dropout(0.5))
 model.add(Dense(1))
 
+#model = Sequential()
+#model.add(Lambda(lambda x : (x / 255.0) - 0.5, input_shape=(160,320,3)))
+#model.add(Cropping2D(cropping=((70,25),(0,0))))
+#model.add(Conv2D(1 * 6, (5, 5), activation="relu"))
+#model.add(MaxPooling2D())
+#model.add(Conv2D(1 * 16, (5, 5), activation="relu"))
+#model.add(MaxPooling2D())
+##model.add(Dropout(0.5))
+#model.add(Flatten())
+#model.add(Dense(1 * 120,activation="relu"))
+##model.add(Dropout(0.5))
+#model.add(Dense(1 * 84,activation="relu"))
+##model.add(Dropout(0.5))
+#model.add(Dense(1))
+
+
 model.compile(loss='mse', optimizer='adam')
 
 model.fit_generator(
     generator(train_logs),
-    2 * len(train_logs) // batch_size,
-    epochs=5,
+    len(train_logs) // batch_size,
+    epochs=10,
     validation_data = generator(valid_logs),
-    validation_steps = 2 * len(valid_logs) // batch_size)
+    validation_steps = len(valid_logs) // batch_size)
 
 #model.fit(X_train,y_train, validation_split=0.2, shuffle=True, epochs=5)
 
